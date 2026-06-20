e382498ecd79bb8b11b3325512326afc3ad88991
sengshijun <sengshijun@gmail.com>
2026-06-20 23:06:08 +0800

    Update API URL for deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 // frontend/src/utils/api.js
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || '/api',
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('skillswap_token');
